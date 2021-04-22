31052f9b800f1a2a14a5bcbea0be2e9a2b6b2aac
cgaires <charles.ifsul@gmail.com>
2021-04-22 16:05:28 -0300

    Criado um menu com laço while

---
+++ README.md
@@ -0,0 +1,2 @@
+#Apresentação
+A finalidade desse repositório é a criação de um sistema que terá como objetivo a aplicação dos estudos em Java e versionamento, usado assim para criar e compartilhar experiências.
--- src/main/java/Sistema/Sistema.java
+++ src/main/java/Sistema/Sistema.java
@@ -4,26 +4,40 @@ import java.util.Scanner;
 public class Sistema {
     public static void main(String[] args) {
 
-        System.out.println("Olá!\nDigite o número da opção desejada:\n1. Ainda não possuo cadastro\n2. Já possuo cadastro\n3. Sair");
+        String nomeDefinido = "--- Nome não registrado ainda ---";
+
+        int menuConvertido = 0;
+
+        while (menuConvertido != 3) {
+
+        System.out.println("Digite o número da opção desejada:\n1. Ainda não possuo cadastro\n2. Já possuo cadastro\n3. Sair");
 
         Scanner menuRegistrar = new Scanner(System.in);
 
         String menuRegistrado = menuRegistrar.nextLine();
 
+        menuConvertido = Integer.parseInt(menuRegistrado);
 
-        if (menuRegistrado.equals("1")) {
+            if (menuConvertido == 1) {
 
-            System.out.println("Qual o seu nome?");
+                System.out.println("Qual o seu nome?");
 
-            Scanner nomeRegistrar = new Scanner(System.in);
+                Scanner nomeRegistrar = new Scanner(System.in);
 
-            String nomeRegistrado = nomeRegistrar.nextLine();
-        }
-        else if(menuRegistrado.equals("2")){
-            System.out.println("Seu nome é " + menuRegistrado);
-        }
-        else {
-            System.out.println("Saindo do sistema...\nAté logo :)");
+                String nomeRegistrado = nomeRegistrar.nextLine();
+
+                System.out.println("Nome registrado com sucesso\nVoltando ao menu...\n");
+
+                nomeDefinido = nomeRegistrado;
+
+            } else if (menuConvertido == 2) {
+                System.out.println("Seu nome é " + nomeDefinido + "\n");
+            } else if (menuConvertido ==3) {
+                System.out.println("Saindo do sistema...\nAté logo :)");
+
+            } else {
+                System.out.println("Opção inválida, por favor tente novamente digitando apenas os números apresentados no menu\n");
+            }
         }
     }
 }
